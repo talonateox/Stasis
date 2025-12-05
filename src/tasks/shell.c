fd5ae46646bad98adcc9f9fa9538bd3ad82e8177
talonateox <back.up.email.158@gmail.com>
2025-12-05 04:25:11 -0600

    basic command interface

--- src/tasks/shell.c
+++ src/tasks/shell.c
@@ -2,20 +2,39 @@
 
 #include "../io/terminal.h"
 #include "../drivers/keyboard/keyboard.h"
+#include "../std/string.h"
+
+#define INPUT_BUFFER_SIZE 256
+
+static char input_buffer[INPUT_BUFFER_SIZE];
+static size_t index = 0;
+
+void process_command() {
+    if(memcmp(input_buffer, "test", 4) == 0) {
+        printkf("test command executed!\n");
+    } else {
+        input_buffer[index-1] = '\0';
+        printkf("Command '%s' not found\n", input_buffer);
+    }
+    memset(input_buffer, 0, index);
+    index = 0;
+}
 
 void shell() {
     const char* ps1 = "shell> ";
     printkf("STASIS INTERACTIVE SHELL\n\n");
 
-    printkf("%s", ps1);
+    printkf("%k%s%r", 0x89cff0, ps1);
 
     while(1) {
         char c = keyboard_getchar();
 
         putkc(c);
+        input_buffer[index++] = c;
 
         if(c == '\n') {
-            printkf("%s", ps1);
+            process_command();
+            printkf("%k%s%r", 0x89cff0, ps1);
         }
     }
 }
