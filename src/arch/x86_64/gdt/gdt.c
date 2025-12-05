7c8986f7395827604e3161955d07d68c122f4f3d
talonateox <back.up.email.158@gmail.com>
2025-12-05 15:57:48 -0600

    user mode and tss

--- src/arch/x86_64/gdt/gdt.c
+++ src/arch/x86_64/gdt/gdt.c
@@ -1,18 +1,59 @@
 #include "gdt.h"
+#include "gdt.h"
+
+tss_t _g_tss = {0};
 
 __attribute__((aligned(0x1000)))
 gdt_t _g_gdt = {
     .null = {0, 0, 0, 0x00, 0x00, 0},
-    .kernel_code = {0, 0, 0, 0x9a, 0xa0, 0},
-    .kernel_data = {0, 0, 0, 0x92, 0xa0, 0},
-    .user_null = {0, 0, 0, 0x00, 0x00, 0},
-    .user_code = {0, 0, 0, 0x9a, 0xa0, 0},
-    .user_data = {0, 0, 0, 0x92, 0xa0, 0},
+    .kernel_code = {0, 0, 0, 0x9A, 0xA0, 0},
+    .kernel_data = {0, 0, 0, 0x92, 0xA0, 0},
+    .user_data = {0, 0, 0, 0xF2, 0xA0, 0},
+    .user_code = {0, 0, 0, 0xFA, 0xA0, 0},
+    .tss = {0, 0, 0, 0x89, 0x00, 0, 0, 0},
 };
 
-void gdt_init() {
+static void setup_tss_descriptor(void) {
+    uint64_t tss_addr = (uint64_t)&_g_tss;
+    uint32_t tss_size = sizeof(tss_t) - 1;
+
+    _g_gdt.tss.limit0 = tss_size & 0xFFFF;
+    _g_gdt.tss.base0 = tss_addr & 0xFFFF;
+    _g_gdt.tss.base1 = (tss_addr >> 16) & 0xFF;
+    _g_gdt.tss.access = 0x89;
+    _g_gdt.tss.limit1_flags = ((tss_size >> 16) & 0x0F);
+    _g_gdt.tss.base2 = (tss_addr >> 24) & 0xFF;
+    _g_gdt.tss.base3 = (tss_addr >> 32) & 0xFFFFFFFF;
+    _g_gdt.tss.reserved = 0;
+}
+
+void gdt_init(void) {
+    _g_tss.reserved0 = 0;
+    _g_tss.rsp0 = 0;
+    _g_tss.rsp1 = 0;
+    _g_tss.rsp2 = 0;
+    _g_tss.reserved1 = 0;
+    _g_tss.ist1 = 0;
+    _g_tss.ist2 = 0;
+    _g_tss.ist3 = 0;
+    _g_tss.ist4 = 0;
+    _g_tss.ist5 = 0;
+    _g_tss.ist6 = 0;
+    _g_tss.ist7 = 0;
+    _g_tss.reserved2 = 0;
+    _g_tss.reserved3 = 0;
+    _g_tss.iopb_offset = sizeof(tss_t);
+
+    setup_tss_descriptor();
+
     gdt_desc_t desc;
     desc.size = sizeof(gdt_t) - 1;
     desc.offset = (uint64_t)&_g_gdt;
     load_gdt(&desc);
+
+    asm volatile("ltr %w0" : : "r"((uint16_t)GDT_TSS));
+}
+
+void tss_set_kernel_stack(uint64_t stack) {
+    _g_tss.rsp0 = stack;
 }
--- src/tasks/shell.c
+++ src/tasks/shell.c
@@ -18,20 +18,19 @@ void process_command() {
     if(memcmp(input_buffer, "help", 4) == 0) {
         printkf("STASIS SHELL COMMANDS:\n");
         printkf("    mem    - display memory usage\n");
-        printkf("    lstask - display current running tasks\n");
+        printkf("    ps     - display current running tasks\n");
         printkf("    lspci  - enumerate pci devices\n");
         printkf("    clear  - clears the screen\n");
     } else if(memcmp(input_buffer, "mem", 3) == 0) {
         printkf("FREE RAM: %k%llu%k\n", 0xcccc66, pfallocator_get_free_ram(), 0xffffff);
         printkf("USED RAM: %k%llu%k\n", 0xcccc66, pfallocator_get_used_ram(), 0xffffff);
-    } else if(memcmp(input_buffer, "lstask", 6) == 0) {
+    } else if(memcmp(input_buffer, "ps", 2) == 0) {
         scheduler_print_tasks();
     }  else if(memcmp(input_buffer, "lspci", 5) == 0) {
         pci_enumerate(acpi_get_mcfg());
     }  else if(memcmp(input_buffer, "clear", 5) == 0) {
         terminal_clear();
     } else {
-        input_buffer[index-1] = '\0';
         printkf("command '%s' not found\ntype 'help' to see a list of commands\n", input_buffer);
     }
     memset(input_buffer, 0, index);
@@ -48,11 +47,17 @@ void shell() {
         char c = keyboard_getchar();
 
         putkc(c);
-        input_buffer[index++] = c;
 
-        if(c == '\n') {
+        if(c == '\b') {
+            if(index > 0) {
+                index--;
+                input_buffer[index] = '\0';
+            }
+        } else if(c == '\n') {
             process_command();
             printkf("%k%s%r", 0x89cff0, ps1);
+        } else {
+            input_buffer[index++] = c;
         }
     }
 }
